6ffcb348cd4a5d9e55a39c2e578ea3b136609eb8
ZayCodes191 <203040090@mail.unpas.ac.id>
2022-10-21 14:11:25 +0700

    Laravel Struktur Folder, Routes, View

--- applications/coba-laravel/routes/web.php
+++ applications/coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Muhammad Anendha Zaska",
+        "email" => "zaskanovar@gmail.com",
+        "image" => "profil.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });

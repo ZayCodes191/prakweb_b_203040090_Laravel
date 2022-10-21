f8d159601d7a84b4a6647d67a5bb57ea7921b9b8
ZayCodes191 <203040090@mail.unpas.ac.id>
2022-10-21 15:31:01 +0700

    Laravel Blade Templating Engine

--- applications/coba-laravel/routes/web.php
+++ applications/coba-laravel/routes/web.php
@@ -14,17 +14,70 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
-        "name" => "Muhammad Anendha Zaska",
-        "email" => "zaskanovar@gmail.com",
+        "title" => "About",
+        "name" => "Muhamad Anendha Zaska",
+        "email" => "203040090@mail.unpas.ac.id",
         "image" => "profil.jpg"
     ]);
 });
 
+
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+        "title" => "Judul Post Pertama",
+        "slug" => "judul-post-pertama",
+        "author" => "Muhamad Zay",
+        "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, quo! Quibusdam reiciendis ex cupiditate dignissimos, numquam facere adipisci cumque quaerat distinctio libero laboriosam pariatur, natus laborum! Blanditiis deserunt odio fuga neque ratione rerum fugit similique qui necessitatibus? Dignissimos reprehenderit itaque quod ad, temporibus suscipit quisquam repellendus, similique possimus ut dolorum."
+        ],
+        [
+        "title" => "Judul Post Kedua",
+        "slug" => "judul-post-kedua",
+        "author" => "Zay",
+        "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo obcaecati quibusdam saepe quam hic sunt. Dolorem ratione minus recusandae nisi sunt, quas, beatae ea culpa aliquam ipsum nihil saepe quidem debitis nam temporibus delectus quo. Eveniet accusamus cupiditate iure nostrum corporis culpa quasi aperiam quos laborum officia eaque doloremque deserunt atque praesentium, natus quo ullam illum neque modi unde voluptatum veniam accusantium esse! Ipsam animi reiciendis possimus dolorem debitis consequatur aspernatur, voluptatibus obcaecati sed facilis eveniet temporibus eos, unde veritatis laudantium. Fugiat, facere velit? Enim consectetur explicabo quasi nostrum, excepturi qui eaque sunt nesciunt. Molestias aliquam aperiam consectetur facere corrupti?"
+        ],
+    ];
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+// Halaman Single Post
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+        "title" => "Judul Post Pertama",
+        "slug" => "judul-post-pertama",
+        "author" => "Muhamad Anendha Zaska",
+        "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, quo! Quibusdam reiciendis ex cupiditate dignissimos, numquam facere adipisci cumque quaerat distinctio libero laboriosam pariatur, natus laborum! Blanditiis deserunt odio fuga neque ratione rerum fugit similique qui necessitatibus? Dignissimos reprehenderit itaque quod ad, temporibus suscipit quisquam repellendus, similique possimus ut dolorum."
+        ],
+        [
+        "title" => "Judul Post Kedua",
+        "slug" => "judul-post-kedua",
+        "author" => "Zay",
+        "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo obcaecati quibusdam saepe quam hic sunt. Dolorem ratione minus recusandae nisi sunt, quas, beatae ea culpa aliquam ipsum nihil saepe quidem debitis nam temporibus delectus quo. Eveniet accusamus cupiditate iure nostrum corporis culpa quasi aperiam quos laborum officia eaque doloremque deserunt atque praesentium, natus quo ullam illum neque modi unde voluptatum veniam accusantium esse! Ipsam animi reiciendis possimus dolorem debitis consequatur aspernatur, voluptatibus obcaecati sed facilis eveniet temporibus eos, unde veritatis laudantium. Fugiat, facere velit? Enim consectetur explicabo quasi nostrum, excepturi qui eaque sunt nesciunt. Molestias aliquam aperiam consectetur facere corrupti?"
+        ],
+    ];
+
+
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
